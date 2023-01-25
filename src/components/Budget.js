f9a7fdafb2a0aa0a34b64b30ea05654d69ca980e
agenefriday <agenedrillerz@gmail.com>
2023-01-25 16:45:14 +0100

    Made some changes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-  const { budget } = useContext(AppContext);
+  const { budget, expenses } = useContext(AppContext);
+  const [budget_val, setBudget_val] = useState(budget);
 
   return (
     <div className='alert alert-secondary'>
-      <span>Budget: £{budget}</span>
+      <span>
+        Budget: £
+        <input
+          type='number'
+          value={budget_val}
+          onChange={(e) => {
+            e.target.value > 20000
+              ? window.alert("Value can not be more than 20,000")
+              : setBudget_val(e.target.value);
+
+            e.target.value < expenses
+              ? window.alert("Value can not be less than spending")
+              : setBudget_val(e.target.value);
+          }}
+        />
+      </span>
     </div>
   );
 };
